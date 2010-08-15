1893c6cdc6dfb2c45bafe99de0f52e0eac8d5e1a
synhershko <itamar@code972.com>
2010-08-15 22:59:26 +0300

    Licensing and attribution

--- src/NAppUpdate.Framework/Properties/AssemblyInfo.cs
+++ src/NAppUpdate.Framework/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NAppUpdate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NAppUpdate - .NET Application Updater")]
+[assembly: AssemblyDescription("Library for easy and flexible integration of application update mechanism")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Itamar Syn-Hershko")]
 [assembly: AssemblyProduct("NAppUpdate")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright © 2010 Itamar Syn-Hershko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- src/NAppUpdate.Updater/Properties/AssemblyInfo.cs
+++ src/NAppUpdate.Updater/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle(".NET Application Updater")]
-[assembly: AssemblyDescription("Update executable for updating WPF applications")]
+[assembly: AssemblyTitle("NAppUpdate - .NET Application Updater")]
+[assembly: AssemblyDescription("Update executable for updating .NET applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("leetreveil")]
+[assembly: AssemblyCompany("Itamar Syn-Hershko")]
 [assembly: AssemblyProduct("NAppUpdate.Updater")]
-[assembly: AssemblyCopyright("Copyright ©  2010 Lee Treveil")]
+[assembly: AssemblyCopyright("Copyright © 2010 Itamar Syn-Hershko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -41,5 +41,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
