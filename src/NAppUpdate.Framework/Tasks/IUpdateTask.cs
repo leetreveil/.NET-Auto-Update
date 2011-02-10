4dafc4dc65ce4e504fca0378db00d338e8ec84ca
synhershko <itamar@code972.com>
2011-02-10 18:48:58 +0200

    A bit of documentation

--- src/NAppUpdate.Framework/Tasks/IUpdateTask.cs
+++ src/NAppUpdate.Framework/Tasks/IUpdateTask.cs
@@ -12,8 +12,25 @@ namespace NAppUpdate.Framework.Tasks
         string Description { get; set; }
         BooleanCondition UpdateConditions { get; set; }
 
+        /// <summary>
+        /// Do all work, especially if it is lengthy, required to prepare the update task, except from
+        /// the final trivial operations required to actually perform the update.
+        /// </summary>
+        /// <param name="source">An update source object, in case more data is required</param>
+        /// <returns>True if successful, false otherwise</returns>
         bool Prepare(NAppUpdate.Framework.Sources.IUpdateSource source);
+        
+        /// <summary>
+        /// Execute the update. After all preparation is done, this call should be quite a short one
+        /// to perform.
+        /// </summary>
+        /// <returns>True if successful, false otherwise</returns>
         bool Execute();
+
+        /// <summary>
+        /// Rollback the update performed by this task.
+        /// </summary>
+        /// <returns>True if successful, false otherwise</returns>
         bool Rollback();
     }
 }
