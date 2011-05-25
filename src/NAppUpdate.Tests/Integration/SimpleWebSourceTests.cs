6b70a0c0985707b0b9bb920383655fdd0b610412
synhershko <itamar@code972.com>
2011-05-25 17:27:03 +0300

    Adding passing test

---
+++ src/NAppUpdate.Tests/Integration/SimpleWebSourceTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NAppUpdate.Framework.Sources;
+
+namespace NAppUpdate.Tests.Integration
+{
+	[TestClass]
+	public class SimpleWebSourceTests
+	{
+		public void can_download_non_utf_feed()
+		{
+			const string expected = "NHibernate.Profiler-Build-";
+
+			var ws = new SimpleWebSource("http://builds.hibernatingrhinos.com/latest/nhprof");
+			var str = ws.GetUpdatesFeed();
+
+			Assert.AreEqual(expected, str.Substring(0, expected.Length));
+		}
+	}
+}
