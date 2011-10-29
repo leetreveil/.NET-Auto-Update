df22b624e156920e88419d8225d6f522ba99c23a
Itamar Syn-Hershko <itamar@code972.com>
2011-10-29 20:56:04 +0200

    Adding tests

--- src/NAppUpdate.Tests/FeedReaders/NauXmlFeedReaderTests.cs
+++ src/NAppUpdate.Tests/FeedReaders/NauXmlFeedReaderTests.cs
@@ -13,8 +13,11 @@ namespace NAppUpdate.Tests.FeedReaders
     [TestClass]
     public class NauXmlFeedReaderTests
     {
-        const string NauUpdateFeed =
-                @"<?xml version=""1.0"" encoding=""utf-8""?>
+        [TestMethod]
+        public void NauReaderCanReadFeed1()
+        {
+        	const string NauUpdateFeed =
+        		@"<?xml version=""1.0"" encoding=""utf-8""?>
 <Feed>
   <Title>My application</Title>
   <Link>http://myapp.com/</Link>
@@ -28,13 +31,48 @@ namespace NAppUpdate.Tests.FeedReaders
   </Tasks>
 </Feed>";
 
-        [TestMethod]
-        public void TestNauReaderCanReadFeed()
-        {
             var reader = new NAppUpdate.Framework.FeedReaders.NauXmlFeedReader();
             IList<IUpdateTask> updates = reader.Read(NauUpdateFeed);
 
-            Assert.IsTrue(updates.Count > 0);
+            Assert.IsTrue(updates.Count == 1);
+
+        	var task = updates[0] as FileUpdateTask;
+			Assert.IsNotNull(task);
+
+			Assert.AreEqual(1, task.UpdateConditions.ChildConditionsCount);
+
+			Assert.IsFalse(task.CanHotSwap);
         }
+
+		[TestMethod]
+		public void NauReaderCanReadFeed2()
+		{
+			const string NauUpdateFeed =
+				@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Feed>
+  <Title>My application</Title>
+  <Link>http://myapp.com/</Link>
+  <Tasks>
+    <FileUpdateTask localPath=""test.dll"" updateTo=""remoteFile.dll"" hotswap=""true"">
+      <Description>update details</Description>
+      <Conditions>
+        <FileExistsCondition localPath=""otherFile.dll"" />
+      </Conditions>
+    </FileUpdateTask>
+  </Tasks>
+</Feed>";
+
+			var reader = new NAppUpdate.Framework.FeedReaders.NauXmlFeedReader();
+			IList<IUpdateTask> updates = reader.Read(NauUpdateFeed);
+
+			Assert.IsTrue(updates.Count == 1);
+
+			var task = updates[0] as FileUpdateTask;
+			Assert.IsNotNull(task);
+
+			Assert.AreEqual(1, task.UpdateConditions.ChildConditionsCount);
+
+			Assert.IsTrue(task.CanHotSwap);
+		}
     }
 }
