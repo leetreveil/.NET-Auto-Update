63f1737b506d5e312b7a08adcc5ca762f958b9d3
Itamar Syn-Hershko <itamar@code972.com>
2011-10-29 21:12:08 +0200

    More tests

--- src/NAppUpdate.Tests/FeedReaders/NauXmlFeedReaderTests.cs
+++ src/NAppUpdate.Tests/FeedReaders/NauXmlFeedReaderTests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NAppUpdate.Framework;
+using NAppUpdate.Framework.Conditions;
 using NAppUpdate.Framework.Tasks;
 
 namespace NAppUpdate.Tests.FeedReaders
@@ -38,10 +39,17 @@ namespace NAppUpdate.Tests.FeedReaders
 
         	var task = updates[0] as FileUpdateTask;
 			Assert.IsNotNull(task);
+			Assert.IsFalse(task.CanHotSwap);
+			Assert.AreEqual("test.dll", task.LocalPath);
+			Assert.AreEqual("remoteFile.dll", task.UpdateTo);
+			Assert.IsNull(task.Sha256Checksum);
+			Assert.IsNotNull(task.Description);
 
 			Assert.AreEqual(1, task.UpdateConditions.ChildConditionsCount);
 
-			Assert.IsFalse(task.CanHotSwap);
+        	var cnd = task.UpdateConditions.Degrade() as FileExistsCondition;
+			Assert.IsNotNull(cnd);
+			Assert.AreEqual("otherFile.dll", cnd.LocalPath);
         }
 
 		[TestMethod]
@@ -56,7 +64,7 @@ namespace NAppUpdate.Tests.FeedReaders
     <FileUpdateTask localPath=""test.dll"" updateTo=""remoteFile.dll"" hotswap=""true"">
       <Description>update details</Description>
       <Conditions>
-        <FileExistsCondition localPath=""otherFile.dll"" />
+        <FileVersionCondition what=""below"" version=""1.0.176.0"" />
       </Conditions>
     </FileUpdateTask>
   </Tasks>
@@ -69,10 +77,16 @@ namespace NAppUpdate.Tests.FeedReaders
 
 			var task = updates[0] as FileUpdateTask;
 			Assert.IsNotNull(task);
+			Assert.IsTrue(task.CanHotSwap);
 
 			Assert.AreEqual(1, task.UpdateConditions.ChildConditionsCount);
 
-			Assert.IsTrue(task.CanHotSwap);
+			var cnd = task.UpdateConditions.Degrade() as FileVersionCondition;
+			Assert.IsNotNull(cnd);
+			Assert.IsNull(cnd.LocalPath);
+
+			Assert.AreEqual("below", cnd.ComparisonType);
+			Assert.AreEqual("1.0.176.0", cnd.Version);
 		}
     }
 }
