fb4ddb803d8270003b85f0fd27ecc84944b464c6
Drew Keller <drew@drewkeller.com>
2012-05-25 00:25:45 -0500

    Add a button to the main window so checking can be testing more than once. However, the update only seems to fire the first time it's called.

--- Samples/WPFSampleApp/MainWindow.xaml.cs
+++ Samples/WPFSampleApp/MainWindow.xaml.cs
@@ -21,24 +21,41 @@ namespace NAppUpdate.SampleApp
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //CheckForUpdates();
+        }
+
+        private void btnCheckForUpdates_Click(object sender, RoutedEventArgs e)
+        {
+            //UpdateManager updManager = UpdateManager.Instance;
+            //updManager.Abort();
+            CheckForUpdates();
+        }
+
+        private void CheckForUpdates()
+        {
             UpdateManager updManager = UpdateManager.Instance;
 
             //update configuration
             updManager.UpdateFeedReader = new NAppUpdate.Framework.FeedReaders.AppcastReader();
-            updManager.UpdateSource = new NAppUpdate.Framework.Sources.SimpleWebSource();
+            //updManager.UpdateSource = new NAppUpdate.Framework.Sources.SimpleWebSource();
+            updManager.UpdateSource = new NAppUpdate.Framework.Sources.MemorySource(File.ReadAllText("SampleAppUpdateFeed.xml"));
 
-            updManager.CheckForUpdateAsync(new NAppUpdate.Framework.Sources.MemorySource(File.ReadAllText("sampleappupdatefeed.xml"))
-                , updatesCount =>
+            updManager.CheckForUpdateAsync(
+                updatesCount =>
                 {
-                    Action showUpdateAction = () => new UpdateWindow(UpdateManager.Instance).Show();
-
-                    if (updatesCount > 0)
+                    try
                     {
-                        if (Dispatcher.CheckAccess())
-                            showUpdateAction();
-                        else
-                            Dispatcher.Invoke(showUpdateAction);
+                        Action showUpdateAction = () => new UpdateWindow(UpdateManager.Instance).Show();
+
+                        if (updatesCount > 0)
+                        {
+                            if (Dispatcher.CheckAccess())
+                                showUpdateAction();
+                            else
+                                Dispatcher.Invoke(showUpdateAction);
+                        }
                     }
+                    catch (Exception ex) { MessageBox.Show("Could not start UpdateWindow: \n" + ex.Message); }
                 });
         }
     }
