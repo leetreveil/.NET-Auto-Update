b127c7fffc44381b23920091d15a55b3e8b6651a
Itamar Syn-Hershko <itamar@code972.com>
2012-07-03 04:55:17 +0300

    TODO

--- src/NAppUpdate.Framework/Sources/IUpdateSource.cs
+++ src/NAppUpdate.Framework/Sources/IUpdateSource.cs
@@ -5,7 +5,7 @@ namespace NAppUpdate.Framework.Sources
 {
     public interface IUpdateSource
     {
-        string GetUpdatesFeed();
+        string GetUpdatesFeed(); // TODO: return a the feed as a stream
 		bool GetData(string filePath, string basePath, Action<UpdateProgressInfo> onProgress, ref string tempLocation);
     }
 }
