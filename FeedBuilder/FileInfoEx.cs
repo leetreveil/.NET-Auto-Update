f1a81d7a494ef4bd45d221ce286d940ebb1fbe6c
Itamar Syn-Hershko <itamar@code972.com>
2014-08-04 20:57:34 +0300

    Version replace , for . , closes #41

--- FeedBuilder/FileInfoEx.cs
+++ FeedBuilder/FileInfoEx.cs
@@ -31,6 +31,7 @@ namespace FeedBuilder
 		{
 			myFileInfo = new FileInfo(fileName);
 			myFileVersion = FileVersionInfo.GetVersionInfo(fileName).FileVersion;
+			if (myFileVersion != null) myFileVersion = myFileVersion.Replace(", ", ".");
 			myHash = NAppUpdate.Framework.Utils.FileChecksum.GetSHA256Checksum(fileName);
             RelativeName = fileName.Substring(rootDirLength + 1);
 		}
